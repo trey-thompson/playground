01edd08a1d91076df8d29bd793e25e7711a6d7aa
Trey Thompson <trey.thompson413@gmail.com>
2022-12-01 15:02:11 -0600

    Added 8 item cards to the sortable view

---
+++ frontend/src/components/UIkitSortable/SortableList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <ul class="uk-grid-small uk-child-width-1-2 uk-child-width-1-4@s" uk-sortable="handle: .uk-card" uk-grid>
+      <li>
+        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 1</div>
+      </li>
+      <li>
+        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 2</div>
+      </li>
+      <li>
+        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 3</div>
+      </li>
+      <li>
+        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 4</div>
+      </li>
+      <li>
+        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 5</div>
+      </li>
+      <li>
+        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 6</div>
+      </li>
+      <li>
+        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 7</div>
+      </li>
+      <li>
+        <div class="uk-card uk-card-default uk-card-body uk-text-center">Item 8</div>
+      </li>
+    </ul>
+    <!-- <sortable-item
+      v-for="(item) in sortableItems"
+      v-bind:key="item.id"
+      class="uk-card uk-card default uk-card-body uk-text-center"
+    /> -->
+  </div>
+</template>
+
+<script>
+// import SortableItem from './SortableItem.vue'
+export default {
+  name: 'sortable-list',
+  // components: { SortableItem },
+  data() {
+    return {
+      sortableItems: Array(8),
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/views/UIkitSortableView.vue
+++ frontend/src/views/UIkitSortableView.vue
@@ -2,12 +2,17 @@
   <div>
     <title>UIkit - Sortable - Playground</title>
     <h1>UIkit - Sortable</h1>
+    <sortable-list/>
   </div>
 </template>
 
 <script>
+import SortableList from '@/components/UIkitSortable/SortableList.vue'
 export default {
-
+  name: 'uikit-sortable-view',
+  components: {
+    SortableList
+  },
 }
 </script>
 
